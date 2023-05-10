24e237a5425f6119896cc68bceacf5bb32ee1283
Maria Bychkova <mar.byckova@gmail.com>
2023-05-10 13:13:28 +0300

    initial Commit

---
+++ CombSortLib/CombSort.cs
@@ -0,0 +1,21 @@
+﻿namespace CombSortLib;
+public class CombSort
+{
+    public int[] BubbleSort(int[] arrayToSort)
+    {
+        for (int i = 1; i < arrayToSort.Length; i++)
+        {
+            for (int j = 1; j < arrayToSort.Length; j++)
+            {
+                if (arrayToSort[j] < arrayToSort[j - 1])
+                {
+                    int temp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j - 1];
+                    arrayToSort[j - 1] = temp;
+                }
+            }
+        }
+        return arrayToSort;
+    }
+}
+

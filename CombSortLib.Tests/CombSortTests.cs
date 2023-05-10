8d26c29b6878f923dcb5cd71559f9e255180d1d9
Maria Bychkova <mar.byckova@gmail.com>
2023-05-10 13:27:42 +0300

    Added tests directory

---
+++ CombSortLib.Tests/CombSortTests.cs
@@ -0,0 +1,15 @@
+﻿namespace CombSortLib.Tests;
+
+public class CombSortTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
